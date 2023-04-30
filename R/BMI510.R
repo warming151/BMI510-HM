42d53d3c4ee4335514f8202fed36b1bfd5d33be8
Min Huang <64031030+warming151@users.noreply.github.com>
2023-04-30 18:52:30 -0400

    Add files via upload

---
+++ R/BMI510.R
@@ -0,0 +1,331 @@
+
+# bmi510.R
+#' rando
+#'
+#' @param x
+#' @param n, replace
+#'
+#' @return
+#' @export
+#'
+#' @examples
+rando = function(x, n=1, replace=T){
+  if (is.atomic(x)) {
+    return(sample(x, n, replace=replace))
+  } else if (is.data.frame(x)) {
+    return(x[sample(nrow(x), n, replace=replace), ])
+  }
+}
+
+
+#' is_min
+#'
+#' @param x
+#' @param na.rm
+#'
+#' @return
+#' @export
+#'
+#' @examples
+is_min = function(x, na.rm = T) {
+  if (na.rm) {
+    x = na.omit(x)
+  }
+  min_x = min(x)
+  return(x == min_x)
+}
+
+#' is_max
+#'
+#' @param x
+#' @param na.rm
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+is_max = function(x, na.rm = T) {
+  if (na.rm) {
+    x = na.omit(x)
+  }
+  max_x = max(x)
+  return(x == max_x)
+}
+
+#' rep_mat
+#'
+#' @param x
+#' @param M,N
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+rep_mat = function(x, M=1, N=1){
+  if (N > 1) {
+    x = t(x)
+  }
+  rep_x = matrix(NA, nrow=nrow(x)*M, ncol=ncol(x)*N)
+  for (i in 1:nrow(x)) {
+    for (j in 1:ncol(x)) {
+      rep_x[(i-1)*M+1:i*M, (j-1)*N+1:j*N] = x[i,j]
+    }
+  }
+  if (N > 1) {
+    rep_x = t(rep_x)
+  }
+  return(rep_x)
+}
+
+
+#' classes
+#'
+#' @param x
+#' @param
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+classes = function(x) {
+  if (!inherits(x, "tbl_df") && !inherits(x, "data.frame")) {
+    stop("x must be a tibble or data frame")
+  }
+  return(sapply(x, class))
+}
+
+#' df_scale
+#'
+#' @param x
+#' @param center,scale
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+df_scale = function(x, center = T, scale = T) {
+  num_cols = sapply(x, is.numeric)
+  if (any(num_cols)) {
+    x[num_cols] = scale(x[num_cols], center = center, scale = scale)
+  }
+  attr(x, "class") = classes(x)
+  return(x)
+}
+
+
+#' log_likelihood_norm
+#'
+#' @param x
+#' @param mean,sd
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+log_likelihood_norm = function(x, mean, sd) {
+  -sum(dnorm(x, mean, sd, log = TRUE))
+}
+
+#' log_likelihood_unif
+#'
+#' @param x
+#' @param min,max
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+log_likelihood_unif = function(x, min, max) {
+  -sum(dunif(x, min, max, log = TRUE))
+}
+
+#' log_likelihood_chisq
+#'
+#' @param x
+#' @param df
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+log_likelihood_chisq = function(x, df) {
+  -sum(dchisq(x, df, log = TRUE))
+}
+
+#' log_likelihood_f
+#'
+#' @param x
+#' @param df1,df2
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+log_likelihood_f = function(x, df1, df2) {
+  -sum(df1/2*log(df1/2) + df2/2*log(df2/2) + (df1/2-1)*log(x) - ((df1+df2)/2)*log(df2+df1*x/df2) - lgamma(df1/2) - lgamma(df2/2) + lgamma((df1+df2)/2))
+}
+
+#' log_likelihood_t
+#'
+#' @param x
+#' @param df
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+log_likelihood_t = function(x, df) {
+  -sum(dt(x, df, log = TRUE))
+}
+
+#' sensitivity
+#'
+#' @param pred
+#' @param truth
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+sensitivity = function(pred, truth) {
+  sum(pred == 1 & truth == 1) / sum(truth == 1)
+}
+
+#' specificity
+#'
+#' @param pred
+#' @param truth
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+specificity = function(pred, truth) {
+  sum(pred == 0 & truth == 0) / sum(truth == 0)
+}
+
+#' precision
+#'
+#' @param pred
+#' @param truth
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+precision = function(pred, truth) {
+  sum(pred == 1 & truth == 1) / sum(pred == 1)
+}
+
+#' recall
+#'
+#' @param pred
+#' @param truth
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+recall = function(pred, truth) {
+  sum(pred == 1 & truth == 1) / sum(truth == 1)
+}
+
+#' accuracy
+#'
+#' @param pred
+#' @param truth
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+accuracy = function(pred, truth) {
+  mean(pred == truth)
+}
+
+#' f1
+#'
+#' @param pred
+#' @param truth
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+f1 = function(pred, truth) {
+  p = precision(pred, truth)
+  r = recall(pred, truth)
+  2 * (p * r) / (p + r)
+}
+
+#' minimum_n_per_group
+#'
+#' @param d
+#' @param power
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+minimum_n_per_group = function(d, power = 0.8) {
+  es = abs(d)
+  qt = qt(1 - 0.5 * (1 - power), df = 2 * es^2)
+  n = (2 * qt / d)^2
+  return(ceiling(n))
+}
+
+
+#' r2
+#'
+#' @param pred
+#' @param truth
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+r2 = function(pred, truth) {
+  ssr = sum((pred - truth)^2)
+  sst = sum((truth - mean(truth))^2)
+  return(1 - ssr / sst)
+}
+
+
+#' is_max
+#'
+#' @param x
+#' @param na.rm
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+adj_R2 = function(pred, truth, n_p) {
+  n = length(truth)
+  r2 = r2(pred, truth)
+  adj_r2 = 1 - ((1 - r2) * (n - 1)) / (n - n_p - 1)
+  return(adj_r2)
+}
+
+
+
+
+
